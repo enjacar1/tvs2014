e2fd6065bfa49e9603e86bd4ee76f4500a80e11d
tam-09 <tam.th09@gmail.com>
2014-11-07 22:43:31 -0200

    Test suite

---
+++ test/selenium/SuiteTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selenium;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   TestContact.class,
+   TestLogin.class
+})
+public class SuiteTest {
+    
+}	
